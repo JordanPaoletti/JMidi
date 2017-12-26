01bba92b7474fa01c2eadc369f19dcc6d0e170c0
JordanPaoletti <paoletti.jordan@yahoo.com>
2017-12-25 19:23:34 -0700

    Implemented UInt7 and began working on test class for it

--- src/com/jpaoletti/midi/utils/UInt7.java
+++ src/com/jpaoletti/midi/utils/UInt7.java
@@ -1,5 +1,10 @@
 package com.jpaoletti.midi.utils;
 
+/**
+ * Basic implementation on a 7 bit unsigned integer.
+ * primarily meant to be an easy holder of data as opposed to
+ * a comprehensive integer class
+ */
 public class UInt7 implements Comparable<UInt7>{
     /*
     Class Constants
@@ -23,7 +28,7 @@ public class UInt7 implements Comparable<UInt7>{
      * @throws IllegalArgumentException if (value < MIN_VALUE || value > MAX_VALUE)
      */
     public UInt7(int value) {
-        //todo UInt7(int value)
+        setValue(value);
     }
 
     /**
@@ -31,7 +36,7 @@ public class UInt7 implements Comparable<UInt7>{
      * @param o the UInt7 to copy
      */
     public UInt7(UInt7 o) {
-        //todo UInt7(UInt7 o)
+        this.value = o.getValue();
     }
 
     /*
@@ -51,7 +56,10 @@ public class UInt7 implements Comparable<UInt7>{
      * @throws IllegalArgumentException if (value < MIN_VALUE || value > MAX_VALUE)
      */
     public void setValue(int value) {
-        //todo setValue(int value)
+        if (value < MIN_VALUE || value > MAX_VALUE)
+            throw new IllegalArgumentException("Value is out of bounds");
+
+        this.value = (byte) value;
     }
 
     /*
@@ -60,18 +68,27 @@ public class UInt7 implements Comparable<UInt7>{
 
     /**
      * Adds an integer to the value. Overflows simply restart from the beginning
+     * Doesn't handle the case where num + value > Integer.MAX_VALUE
+     *
      * @param num number to sum with
      */
     public void add(int num) {
-        //todo add(int num)
+        value = (byte)((num + value) % (MAX_VALUE + 1));
     }
 
     /**
      * Subs an integer from the value.
+     * Underflow results in a value of 0
+     * if num < 0, then this method calls add
      * @param num number to sub with
      */
     public void sub(int num) {
-        //todo sub(int num)
+        if (num < 0) {
+            add(-num);
+        }
+        else {
+            value = (byte) ((value - num) < 0 ? 0 : value - num);
+        }
     }
 
     /*
@@ -79,7 +96,6 @@ public class UInt7 implements Comparable<UInt7>{
      */
     @Override
     public int compareTo(UInt7 o) {
-        //todo compareTo(UInt7 o)
-        return 0;
+        return (int)value - o.getValue();
     }
 }
